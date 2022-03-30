6a0e920e75dc39fc142aaa984ffe6fbd08ec7854
Denis O <denis.o@linux.com>
2022-03-30 11:40:22 +0300

    added osslingcode image

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-DOCKER_REPO="${DOCKER_REPO:-denis256}"
+DOCKER_REPO="${DOCKER_REPO:-universaldevelopment}"
 PUSH=${PUSH:-0}
 SQUASH=${SQUASH:-0}
 IMAGE="$1"
@@ -11,7 +11,7 @@ cd "$IMAGE"
 
 source config.sh
 
-docker build . -t "$DOCKER_REPO/$IMAGE:$TAG"  --squash
+docker build . -t "$DOCKER_REPO/$IMAGE:$TAG" --squash
 
 if [[ "${PUSH}" == "1" ]]; then
 	docker push "$DOCKER_REPO/$IMAGE:$TAG"
