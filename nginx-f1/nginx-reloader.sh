a9caac29c38619e5af4c323729e7dfe3adccf49e
denis256.dev-desktop <denis.o@linux.com>
2026-01-11 18:31:40 +0000

    reloader update

--- nginx-f1/nginx-reloader.sh
+++ nginx-f1/nginx-reloader.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-# script to watch changes in WATCH_DIR and reload nginx
+# script to watch changes in WATCH_DIR and reload nginx on .conf file changes
 
 export WATCH_DIR=${WATCH_DIR:-/etc/nginx}
 
-echo "[nginx-reloader] Watching directory: ${WATCH_DIR}" >&2
-
-EXCLUDES='.*\\.swp$|.*\\.log$|.*\\.tar\\.gz$'
+echo "[nginx-reloader] Watching directory: ${WATCH_DIR} for *.conf files" >&2
 
 while true
 do
- inotifywait --recursive --exclude "${EXCLUDES}" -e create -e modify -e delete -e move "${WATCH_DIR}"
- nginx -t
- if [[ $? -eq 0 ]]; then
-  echo "Detected Nginx Configuration Change"
-  echo "Executing: nginx -s reload"
-  nginx -s reload
+ FILE=$(inotifywait --recursive --format '%f' -e create -e modify -e delete -e move "${WATCH_DIR}" 2>/dev/null)
+
+ if [[ "${FILE}" == *.conf ]]; then
+  echo "[nginx-reloader] Detected change in: ${FILE}"
+  nginx -t
+  if [[ $? -eq 0 ]]; then
+   echo "[nginx-reloader] Config valid, executing: nginx -s reload"
+   nginx -s reload
+  else
+   echo "[nginx-reloader] Config test failed, skipping reload"
+  fi
  fi
 done
